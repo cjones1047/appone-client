81ff7a9745b8989374ba0b1b472e96915a722fc7
cjones1047 <cjmj1047@gmail.com>
2022-09-08 22:10:16 -0500

    Basic home page (Home component) built

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+
+    return(
+        <div className='mt-5 d-flex justify-content-left' >
+            This is Home Page
+        </div>
+    )
+
+}
+
+export default Home
