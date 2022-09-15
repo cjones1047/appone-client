1fa2dd4787cd7c5541588db9042f9824be64f90a
cjones1047 <cjmj1047@gmail.com>
2022-09-14 19:53:41 -0500

    Simple components for Department, Employee, and Navigation created

---
+++ src/Department.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Department = () => {
+
+    return(
+        <div className='mt-5 d-flex justify-content-left' >
+            This is Department Page
+        </div>
+    )
+
+}
+
+export default Department
---
+++ src/Employee.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Employee = () => {
+
+    return(
+        <div className='mt-5 d-flex justify-content-left' >
+            This is Employee Page
+        </div>
+    )
+
+}
+
+export default Employee
