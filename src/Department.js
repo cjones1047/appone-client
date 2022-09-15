fb087994c2d99179c12a3f6cb146047539bd1336
cjones1047 <cjmj1047@gmail.com>
2022-09-15 11:51:24 -0500

    Api url for Django/Python back-end added to .env file, .env file added to .gitignore, Department component updated to send fetch request to Django API upon mounting and updating and to render data received from Django API response into table format

--- src/Department.js
+++ src/Department.js
@@ -1,10 +1,52 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+
+import { Spinner, Table } from 'react-bootstrap';
 
 const Department = () => {
 
+    const [refresh, setRefresh] = useState(null)
+    const [deps, setDeps] = useState([])
+
+    useEffect(() => {
+        fetch(process.env.REACT_APP_API+'department')
+            .then(response => response.json())
+            .then(data => setDeps(data))
+    }, [refresh])
+
+    if(!deps) return (
+        <Spinner/>
+    )
+
     return(
         <div className='mt-5 d-flex justify-content-left' >
-            This is Department Page
+            <Table
+                className='mt-4'
+                striped
+                bordered
+                hover
+                size='sm'    
+            >
+
+                <thead>
+                    <tr>
+                        <th>DepartmentId</th>
+                        <th>DepartmentName</th>
+                        <th>Options</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {deps.map(dep => 
+                        <tr key={dep.DepartmentId}>
+                            <td>{dep.DepartmentId}</td>
+                            <td>{dep.DepartmentName}</td>
+                            <td>Edit/Delete</td>
+                        </tr>
+                    )}
+                </tbody>
+
+            </Table>
         </div>
     )
 
